fd496cf5cd582800004f148c7802a876740ad8fe
Lauren Guerra <91337957+lauolivia@users.noreply.github.com>
2023-04-06 15:17:06 +0000

    update gred

--- gred_script.py
+++ gred_script.py
@@ -1,9 +1,36 @@
 import os
 
 
-def gred(model, hyperparameters_to_adjust:dict, sigma:list, degree_k:list, ritch_regularization:list,spectral_ratio=1.21,p=0.5,u=6000, ):   
-    
-    ##Example
+def gred(model, hyperparameters_to_adjust:dict, spectral_ratio=1.21,p=0.5,u=6000, threshold=0.1):   
+    params=[]
+    for elem in hyperparameters_to_adjust.values():
+        params.append([ round(elem[3](elem[0],i*elem[2]),10) for i in range(elem[1])])
+
+
+   
+
+
+
+def train(instruction:str):
+    os.system(instruction)
+
+
+def forecast(instruction:str):
+    os.system(instruction)
+
+
+
+hyperparameters_to_adjust={"sigma":(0,5,0.2,lambda x,y: x+y),"degree_k":(2,4,2,lambda x,y: x+y),"ritch_regularization":(10e-4,10,4,lambda x,y: x*y)}
+# gred(hyperparameters_to_adjust=hyperparameters_to_adjust)
+
+params=[]
+for elem in hyperparameters_to_adjust.values():
+    params.append([ round(elem[3](elem[0],i*elem[2]),10) for i in range(elem[1])])
+
+print(params)
+
+
+ ##Example
     # train("python3 ./main.py train \
     #         -m ESN \
     #         -ri WattsStrogatzOwn\
@@ -27,14 +54,16 @@ def gred(model, hyperparameters_to_adjust:dict, sigma:list, degree_k:list, ritch
 
 
 
+# os.system("python3 ./main.py forecast \
+#         -fm classic \
+#         -fl 1000 \
+#         -sil 50\
+#         -tm /home/lauren/Documentos/ESN/trained/dta_'MG_tau16.8_dt0.05_n250000_t-end12500.0_seed2636.csv'-inp_scl_'0.5'-lr_'1.0'-mdl_'ESN'\
+#         -rdout_'linear'-reg_'0.001'-res_deg_'2'-res_std_'0.2'-rw_'0.5'-sp_rad_'1.21'-train_len_'10000'-units_'6000'/saved_model.pb -it 1000 \
+#         -df /home/lauren/Documentos/ESN/data/MG/16.8/MG_tau16.8_dt0.05_n250000_t-end12500.0_seed2636.csv -tr 1000 -tl 10000 \
+#         -o /home/lauren/Documentos/ESN/forecasting")
 
 
-def train(instruction:str):
-    os.system(instruction)
-
-
-def forecast(instruction:str):
-    os.system(instruction)
 
     # os.system("python3 ./main.py forecast -fm classic -fl 1000 -sil 50 -tm '/home/lauren/Documentos/ESN/trained/dta_'MG_tau16.8_dt0.05_n250000_t-end12500.0_seed2636.csv'-inp_scl_'0.5'-lr_'1.0'-mdl_'ESN'-rdout_'linear'-reg_'0.001'-res_deg_'2'-res_std_'0.2'-rw_'0.5'-sp_rad_'1.21'-train_len_'10000'-units_'6000'/saved_model.pb' -o /home/lauren/Documentos/ESN/forecasting -df '/home/lauren/Documentos/ESN/data/MG/16.8/MG_tau16.8_dt0.05_n250000_t-end12500.0_seed2636.csv' -it 1000 -tr 10000 -tl 10000")
     # os.system("python3 ./main.py forecast -fm classic -fl 1000 -sil 50 -tm /home/lauren/Documentos/ESN/trained/dta_'MG_tau16.8_dt0.05_n250000_t-end12500.0_seed2636.csv'-inp_scl_'0.5'-lr_'1.0'-mdl_'ESN'-rdout_'linear'-reg_'0.001'-res_deg_'2'-res_std_'0.2'-rw_'0.5'-sp_rad_'1.21'-train_len_'10000'-units_'6000'/keras_metadata.pb -o /home/lauren/Documentos/ESN/forecasting -df '/home/lauren/Documentos/ESN/data/MG/16.8/MG_tau16.8_dt0.05_n250000_t-end12500.0_seed2636.csv' -it 1000 -tr 10000 -tl 10000")
@@ -42,43 +71,5 @@ def forecast(instruction:str):
 
 # "/home/lauren/Documentos/ESN/trained/dta_'MG_tau16.8_dt0.05_n250000_t-end12500.0_seed2636.csv'-inp_scl_'0.5'-lr_'1.0'-mdl_'ESN'-rdout_'linear'-reg_'0.001'-res_deg_'2'-res_std_'0.2'-rw_'0.5'-sp_rad_'1.21'-train_len_'10000'-units_'6000'"
 # os.system("python3 ./main.py forecast -fm classic  -fl 1000 -sil 50 -tm /home/lauren/Documentos/ESN/trained/dta_'MG_tau16.8_dt0.05_n250000_t-end12500.0_seed2636.csv'-inp_scl_'0.5'-lr_'1.0'-mdl_'ESN'-rdout_'linear'-reg_'0.001'-res_deg_'2'-res_std_'0.2'-rw_'0.5'-sp_rad_'1.21'-train_len_'10000'-units_'6000'/saved_model.pb -it 1000 -df /home/lauren/Documentos/ESN/data/MG/16.8/MG_tau16.8_dt0.05_n250000_t-end12500.0_seed2636.csv -tr 1000 -tl 10000  -o /home/lauren/Documentos/ESN/forecasting")
-
-
-
-
-
-hyperparameters_to_adjust
-
-# gred(sigma=[0.2,0.4,0.6,0.8,1],degree_k=[2,4,6,8],ritch_regularization=[10e-4,10e-5,10e-6,10e-7,10e-8])
-
-
-
-
-
-
-
-    # train("python3 ./main.py train \
-    #         -m ESN \
-    #         -ri WattsStrogatzOwn\
-    #         -df /home/lauren/Documentos/ESN/data/MG/16.8/MG_tau16.8_dt0.05_n250000_t-end12500.0_seed2636.csv \
-    #          -o'/home/lauren/Documentos/ESN/trained'  \
-    #          -rs 0.2 \
-    #          -sr 1.21 \
-    #         -rw 0.5 \
-    #         -u 6000 \
-    #         -rd 2 \
-    #         -rg 10e-4")
-
-
-
-
-os.system("python3 ./main.py forecast \
-        -fm classic \
-        -fl 1000 \
-        -sil 50\
-        -tm /home/lauren/Documentos/ESN/trained/dta_'MG_tau16.8_dt0.05_n250000_t-end12500.0_seed2636.csv'-inp_scl_'0.5'-lr_'1.0'-mdl_'ESN'\
-        -rdout_'linear'-reg_'0.001'-res_deg_'2'-res_std_'0.2'-rw_'0.5'-sp_rad_'1.21'-train_len_'10000'-units_'6000'/saved_model.pb -it 1000 \
-        -df /home/lauren/Documentos/ESN/data/MG/16.8/MG_tau16.8_dt0.05_n250000_t-end12500.0_seed2636.csv -tr 1000 -tl 10000 \
-        -o /home/lauren/Documentos/ESN/forecasting")
 
 
