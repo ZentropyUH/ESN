a5d872ee9b5626755d7112f05b92064959470184
Dennis Fiallo <dfiallo35@gmail.com>
2023-05-23 22:14:34 -0400

    Update base.sh

--- scripts/base.sh
+++ scripts/base.sh
@@ -6,21 +6,16 @@
 
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=100000M
+#SBATCH --mem-per-cpu=10000M
 
-#SBATCH --time=1:00:00
-#SBATCH --partition=short
-
-#SBATCH --array=1-9600
+#SBATCH --time=48:00:00
+#SBATCH --partition=medium
 
 
 
 ########## MODULES ##########
 
 set -e
-module purge
-module load python/3.10.5
-
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 
@@ -32,59 +27,11 @@ scratch="/scratch/$USER/$SLURM_JOB_ID"
 mkdir -p $scratch
 cd $scratch
 
-# project path
-ESN=$scratch/ESN
-mkdir -p $ESN
-
-# output path
-output="$scratch/output"
-mkdir -p $output
-
-# data path
-data="$scratch/data"
-mkdir -p $data
-
-# save path
-save="/data/tsa/destevez/dennis/Lorenz_fixed"
-mkdir -p $save
-
-out="/data/tsa/destevez/dennis/"
-
-config="/data/tsa/destevez/dennis/out.out"
 
 ########## COPY ##########
 
-# Copy project files to scratch
-echo "copying project............"
-# cp -r /data/tsa/destevez/dennis/ESN/* $ESN
 
-echo "copying data............"
-# cp -r /data/tsa/destevez/dennis/Lorenz/* $data
-echo "end of copy"
-
-
-
-########## RUN ##########
-
-case=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $1}' $config)
-
-echo $case
-
-# cd $ESN
-# echo "runing............"
-# srun python3 tmain.py dnfj -p $data -o $output
-# srun python3 tmain.py cf -p $data
-# echo "end of run"
-
-
-
-########## SAVE ##########
-
-# echo "saving............"
-# cp -r $output/* $out
-# cp -r $data/* $save
-# echo "end of save"
-
+rm -rf /data/tsa/destevez/dennis/Lorenz
 
 
 ########## CLEANUP & EXIT ##########
