0b1a3aa6a94b659ddaf63b883cb580ce2b87db1e
Dennis Fiallo <dfiallo35@gmail.com>
2023-05-26 13:35:44 -0400

    Update base.sh

--- scripts/base.sh
+++ scripts/base.sh
@@ -2,13 +2,13 @@
 
 ########## RESOURCES TO USE ##########
 
-#SBATCH --job-name="base"
+#SBATCH --job-name="lorenz"
 
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=10000M
 
-#SBATCH --time=48:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --partition=medium
 
 
@@ -16,6 +16,9 @@
 ########## MODULES ##########
 
 set -e
+module purge
+module load python/3.10.5
+
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 
@@ -27,11 +30,51 @@ scratch="/scratch/$USER/$SLURM_JOB_ID"
 mkdir -p $scratch
 cd $scratch
 
+# project path
+ESN=$scratch/ESN
+mkdir -p $ESN
+
+# output path
+output="$scratch/output"
+mkdir -p $output
+
+# data path
+data="$scratch/data"
+mkdir -p $data
+
+# save path
+save="/data/tsa/destevez/dennis/test_lorenz/"
+mkdir -p $save
+
+
 
 ########## COPY ##########
 
+# Copy project files to scratch
+echo "copying project............"
+cp -r /data/tsa/destevez/dennis/ESN/* $ESN
 
-rm -rf /data/tsa/destevez/dennis/Lorenz
+echo "copying data............"
+cp -r /data/tsa/destevez/data/Lorenz/* $data
+echo "end of copy"
+
+
+
+########## RUN ##########
+
+cd $ESN
+echo "runing............"
+srun python3 main.py grid -i $SLURM_ARRAY_TASK_ID -d $data  -o $output
+echo "end of run"
+
+
+
+########## SAVE ##########
+
+echo "saving............"
+cp -r $output/* $save
+echo "end of save"
+
 
 
 ########## CLEANUP & EXIT ##########
