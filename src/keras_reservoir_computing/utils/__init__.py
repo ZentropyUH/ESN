d5ba2627b56d4708ec3af28408439408303624c9
Daniel Estevez <kemossabee@gmail.com>
2025-02-24 18:26:55 +0100

    Update __init__

--- src/keras_reservoir_computing/utils/__init__.py
+++ src/keras_reservoir_computing/utils/__init__.py
@@ -1,12 +1,11 @@
-from . import data_utils, general_utils, graph_utils, model_utils, plot_utils, tf_utils
+from . import data_utils, general_utils, plot_utils, tf_utils, model_utils
 
 __all__ = [
     "general_utils",
-    "model_utils",
     "data_utils",
     "plot_utils",
     "tf_utils",
-    "graph_utils",
+    "model_utils",
 ]
 
 
