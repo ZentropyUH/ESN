285bcfb514d943d908b81b0642fc5f8c88277c0c
Daniel Estevez <kemossabee@gmail.com>
2025-04-14 12:33:15 +0200

    Added random input matrix

---
+++ src/keras_reservoir_computing/initializers/input_initializers/random_input.py
@@ -0,0 +1,93 @@
+from typing import Optional
+
+import numpy as np
+import tensorflow as tf
+
+
+@tf.keras.utils.register_keras_serializable(package="krc", name="RandomInputInitializer")
+class RandomInputInitializer(tf.keras.Initializer):
+    """Simple random initializer.
+
+    This initializer initializes the weights of the recurrent layer with random values
+    from a uniform distribution between -1 and 1. Controls the spectral radius of the
+    recurrent layer.
+
+    Parameters
+    ----------
+    input_scale : float, optional
+        If provided, the matrix will be scaled to have this input scale.
+    seed : int, optional
+        Random seed for reproducibility.
+
+    Returns
+    -------
+    tf.Tensor
+        A random matrix with the specified shape.
+
+    Notes
+    -----
+    - The matrix is scaled to have the specified spectral radius if provided.
+    
+    Examples
+    --------
+    >>> from keras_reservoir_computing.initializers.input_initializers import RandomInputInitializer
+    >>> initializer = RandomInputInitializer(input_scale=1.0)
+    >>> matrix = initializer((10, 10))
+    >>> print(matrix)
+    """
+    def __init__(
+        self,
+        input_scale: Optional[float] = None,
+        seed: Optional[int] = None,
+    ) -> None:
+        """Initialize the initializer with specified parameters."""
+        self.input_scale = input_scale
+        self.seed = seed
+        self.rng = np.random.default_rng(seed)
+
+    def __call__(self, shape: tuple, dtype=tf.float32) -> tf.Tensor:
+        """Generate a random matrix with the specified shape.
+        
+        Parameters
+        ----------
+        shape : tuple
+            Shape of the matrix to create.
+        
+        Returns
+        -------
+        tf.Tensor
+            Random matrix with the specified shape.
+        """
+        # Generate random values
+        W_in = self.rng.uniform(-1, 1, shape)
+        
+        # Translate the following to numpy
+        if self.input_scale is not None:
+            max_abs_sv = np.max(np.abs(np.linalg.svd(W_in, compute_uv=False)))
+            W_in /= max_abs_sv
+            W_in *= self.input_scale
+        
+        # Convert to tf.Tensor
+        W_in = tf.convert_to_tensor(W_in)
+        
+        # Cast to dtype
+        W_in = tf.cast(W_in, dtype)
+        
+        return W_in
+    
+    def get_config(self) -> dict:
+        """
+        Get the config dictionary of the initializer for serialization.
+
+        Returns
+        -------
+        dict
+            The configuration dictionary.
+        """ 
+        config = {
+            "input_scale": self.input_scale,
+            "seed": self.seed,
+        }
+        base_config = super().get_config()
+        base_config.update(config)
+        return base_config
